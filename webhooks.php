7366bb121215b139402b8a56ff90992e1e1e7aa4
kwsirikun <43022900+kwsirikun@users.noreply.github.com>
2018-09-07 14:24:50 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -20,15 +20,19 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			
-			if $text == '1'  {  $text = 'คุณกด 1' }
-			
-			
+			if $text == '1'  { 
+				
+			$text = 'คุณกด 1' ;
+						
 
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
 				'text' => $text
 			];
+					  
+					  
+			}		  
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
