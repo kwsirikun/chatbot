ce88d654b53d7161ee367926393e26853c2243e9
kwsirikun <43022900+kwsirikun@users.noreply.github.com>
2018-09-07 14:32:23 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -21,7 +21,7 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 			
 			
-			if $text = '1' {			
+			if $text = '1' { echo "1"		}	
 
 			// Build message to reply back
 			$messages = [
@@ -29,7 +29,7 @@ if (!is_null($events['events'])) {
 				'text' => $text
 			];
 				
-				}
+				
 					  
 					  
 
