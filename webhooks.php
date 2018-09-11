72e7640bd8f0e2866ee4b321290b4310aeb65eb5
kwsirikun <43022900+kwsirikun@users.noreply.github.com>
2018-09-11 11:09:04 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -21,7 +21,7 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 			
 			
-			$my_file = 'message.txt';
+			$my_file = 'https://github.com/kwsirikun/chatbot.git/message.txt';
    			$handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
    			$data = 'This is the data';
    			fwrite($handle, $data);
