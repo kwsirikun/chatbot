eb286e87d157beda52f0ab8c70641bafa2aa5b2a
kwsirikun <43022900+kwsirikun@users.noreply.github.com>
2018-09-14 09:39:45 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -49,14 +49,13 @@ if (!is_null($events['events'])) {
 			
 	var database = firebase.database();				
 			
-	function writeUserData(userid, message) {
+	function writeUserData(userId, name, email, imageUrl) {
   	firebase.database().ref('users/' + userId).set({
     	username: name,
     	email: email,
     	profile_picture : imageUrl
   	});
 }
-			
 			
 			
 			
