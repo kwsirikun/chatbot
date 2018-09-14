49edb2e20db5ef72848fdd60c7737768e43086ac
kwsirikun <43022900+kwsirikun@users.noreply.github.com>
2018-09-14 09:39:08 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -36,10 +36,49 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			
+			$userid = $event['source']['userId'];
+			
+			
+			
+////////////////////////////////////////
+			
+						
+			
+			
+<script>
+			
+	var database = firebase.database();				
+			
+	function writeUserData(userid, message) {
+  	firebase.database().ref('users/' + userId).set({
+    	username: name,
+    	email: email,
+    	profile_picture : imageUrl
+  	});
+}
+			
+			
+			
+			
+			
+</script>	
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 			
 			
 
-			
+////////////////////////////////////////////////			
 			
 						
 
